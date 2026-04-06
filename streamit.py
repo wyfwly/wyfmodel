d0df606e79dd365cc3757ee434e5d2daa1d2e304
wyfwly <646205347@qq.com>
2026-04-07 00:01:25 +0800

    Update streamit.py

--- streamit.py
+++ streamit.py
@@ -8,7 +8,7 @@ from autogluon.tabular import TabularPredictor
 import os
 
 #加载保存的模型
-model= TabularPredictor.load("AutogluonModels\ag-20260402_113750/predictor.pkl")
+model= TabularPredictor.load("AutogluonModels\ag-20260402_113750")
 data = pd.read_csv("finaldata.csv")
 
 class AutogluonWrapper:
